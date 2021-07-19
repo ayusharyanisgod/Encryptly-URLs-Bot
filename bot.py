7cf3090675e275d0360d51b01d855edf59f7b4d6
ayusharyanisgod <84650820+ayusharyanisgod@users.noreply.github.com>
2021-07-19 11:40:00 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -47,8 +47,11 @@ async def link_handler(_, update):
         await update.reply(message, quote=True)
         return
     message = f"Here is your shortlink\n {shortened_url}"
-    markup = InlineKeyboardMarkup([[InlineKeyboardButton("Link 🔗", url=shortened_url)]])
-    markup = InlineKeyboardMarkup([[InlineKeyboardButton("Powered By", url="https://t.me/TPVNetwork")]])
+    markup = InlineKeyboardMarkup([[InlineKeyboardButton("Link 🔗", url=shortened_url)],
+                                  ([[InlineKeyboardButton("Powered By", url="https://t.me/TPVNetwork")]])
+                                  ])
+    
+    
     # i don't think this bot with get sending message error so no need of exceptions
     await update.reply_text(text=message, reply_markup=markup, quote=True)
       
